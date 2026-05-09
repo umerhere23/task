15a5d00483885138d24627d3298934bfa2dc8ef7
Umer <omerjh5004@gmail.com>
2026-05-10 02:24:35 +0500

    updated code

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,22 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(_request: NextRequest) {
-  return NextResponse.next();
+function applyCorsHeaders(response: NextResponse) {
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-user-id, x-org-id, x-user-role');
+  response.headers.set('Access-Control-Max-Age', '86400');
+  return response;
+}
+
+export function middleware(request: NextRequest) {
+  if (request.method === 'OPTIONS') {
+    return applyCorsHeaders(new NextResponse(null, { status: 204 }));
+  }
+
+  return applyCorsHeaders(NextResponse.next());
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/api/:path*'],
 };
